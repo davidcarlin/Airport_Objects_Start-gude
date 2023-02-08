2d625c8be343537b56a73fa931a1c5f807b3bd96
davidcarlin <david.carlin@barclays.com>
2023-02-08 23:15:40 +0000

    passed tests

--- Person.js
+++ Person.js
@@ -1,10 +1,18 @@
 class Person {
-    // Include a Constructor Method for Properties: 
-        // 1. Name
-        // 2. Bags
-            // 2a. Bags should contain/reference a data type for storing more than one bag object.
 
-    // Include an addBag method
+    constructor(name, bags) {
+        this.name = name;
+        this.bags = [];
+    }
+
+    addBag(bag) {
+        this.bags.push(bag);
+    }
 }
 
-//Export the Module
+const newPerson = new Person("David");
+const newBag = new Bag(25);
+
+newPerson.addBag(newBag);
+
+module.exports = Person
